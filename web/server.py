57ac2049197daef0f6f4e426a935da7b3ffae7a8
fieryslug <fieryslug@github.com>
2019-10-19 01:09:21 +0800

    m

--- web/server.py
+++ web/server.py
@@ -1,7 +1,8 @@
+import sys
+sys.path.insert(1, '..')
 from flask import Flask, request
-import configs
+import web.configs as configs
 
-import api.utils as utils
 
 app = Flask(__name__)
 context = configs.SSL_CONTEXT

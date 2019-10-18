b23242982b78beec8fb6588f3c1a8d7632d9eb93
fieryslug <fieryslug@github.com>
2019-10-19 07:00:33 +0800

    data fetching

--- api/cache_interface.py
+++ api/cache_interface.py
@@ -17,23 +17,26 @@ def crawl_and_save(loc, date_i, date_f):
     enddate = date_i + delta
 
     while enddate < date_f + delta:
-        r = api_request.make_request_wwo(apis.wwo.historical_local, loc, {'date': date, 'enddate': enddate})
-        d = r['data']
-        dm.remove_url(d)
-
-        k, v = dm.compress(d)
-        res.append(v)
+        r = api_request.make_request_wwo(apis.wwo.historical_local, loc, {'date': date, 'enddate': enddate, 'tp': '6'})
+        if 'weather' in r['data'].keys():
+            d = r['data']['weather']
+            for daily in d:
+                dm.remove_url(daily)
+                k, v = dm.separate(daily)
+                res.append(v)
         print(enddate)
         date = enddate + one_day
         enddate = date + delta
 
     with open('cache/wwo/past-weather/' + loc.lower() + '.json', 'w+') as f:
         json.dump(res, f)
-
     return res
 
 
 def read(loc):
+    res = []
     with open('cache/wwo/past-weather/' + loc + '.json', 'r') as f:
-        res = json.load(f)
-    return dm.assemble(dm.KEY, res)
+        r = json.load(f)
+    for daily in r:
+       res.append(dm.assemble(dm.KEY6, daily))
+    return res
--- sandbox.py
+++ sandbox.py
@@ -1,9 +1,14 @@
 import sys
 sys.path.insert(1, '.')
-from api import api_request, apis, cache_interface
+from api import api_request, apis, cache_interface as ci
 from utils import data_manipulating as dm
 from pprint import pprint
+from constants.countries import countries
 
-r = api_request.make_request_wwo(apis.wwo.historical_local, 'taiwan', {'date': '2010-10-20', 'enddate': '2010-10-24'})
-pprint(r)
 
+N = len(countries)
+
+for i in range(N):
+    country = countries[i]
+    print('{} ({}/{})'.format(country, i+1, N))
+    ci.crawl_and_save(country, '2008-01-01', '2019-09-30')

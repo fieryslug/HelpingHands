a7c979d0667b1a36b175b2f3b0c2ae763b14b60b
18Allen <44614744+18Allen@users.noreply.github.com>
2019-10-19 20:47:38 +0800

    add analysis/color.py,to be integrated to events

---
+++ analysis/color.py
@@ -0,0 +1,32 @@
+# pase the intevral of color. The highter the number, the brighter the color
+def genRGB(init,final,n):
+    color_list = []
+    if n-1==0: return(init)
+
+    a = int(str(final[1:3]),16) - int(str(init[1:3]),16)
+    b = int(str(final[3:5]),16) - int(str(init[3:5]),16)
+    c = int(str(final[5:]),16) - int(str(init[5:]),16)
+    dn = (int(a/n-1),int(b/n-1),int(c/n-1))
+    
+    for i in range(n-1,0,-1):
+        s = '#'
+        s += str(hex(int(str(init[1:3]),16)+i*dn[0]))
+        s += str(hex(int(str(init[3:5]),16)+i*dn[1]))
+        s += str(hex(int(str(init[5:]),16)+i*dn[2]))
+        color_list.append(s)
+    return(color_list)
+
+# mix color with five kind of dataset
+def indexRGB(data1,data2,data3,data4,data5):
+    x = data1*8 + data3*8 + data4*4 + data5*8
+    y = data4*14 + data5*14
+    if x!=0 : 
+       b = 255
+    else :
+       b = x
+    if y!=0 : 
+       r = 255
+    else :
+       r = y
+    s1 = '#' + str(hex(r)[2:]) + str(hex(x+y)[2:]) + str(hex(b)[2:])
+    return(s1)

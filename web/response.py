8ce078f9e34260949d6a2e1d50c3bd6fe7e4261c
fieryslug <fieryslug@github.com>
2019-10-19 17:04:54 +0800

    server

--- web/response.py
+++
@@ -1,8 +0,0 @@
-
-
-class Bubble:
-    def __init__(self, name, long, lat, rad, info):
-        self.name = name
-        self.long = long
-        self.lat = lat
-
--- web/server.py
+++ web/server.py
@@ -7,41 +7,28 @@ from analysis import drought, heat, health
 app = Flask(__name__)
 context = configs.SSL_CONTEXT
 
+
 @app.route('/list.json', methods=['GET', 'POST'])
 def handle_list():
     res = [
         {
             'title': 'Drought',
             'src': '/api.json?dataset=Drought'
-        },
-        {
-            'title': 'HealthCenterDensity',
-            'src': '/api.json?dataset=HealthCenterDensity'
         }
     ]
     return res
 
-@app.route('/api.json', methods=['POST'])
+
+@app.route('/api.json/Drought', methods=['GET'])
 def handle_api():
-    data = request.json
-    theme = data['dataset']
 
     res = dict()
 
-    if theme == 'Drought':
-        res['title'] = 'Drought'
-        res['info'] = drought.info
-        res['geography'] = drought.get_geography()
-        res['bubble'] = drought.get_bubbles()
-        return res
-
-    if theme == 'HealthCenterDensity':
-        res['title'] = 'health center density'
-        res['info'] = health.info
-        res['geography'] = health.get_geography()
-        res['bubble'] = health.get_bubbles()
-
-    return 'error!'
+    res['title'] = 'Drought'
+    res['info'] = drought.info
+    res['geography'] = drought.get_geography()
+    res['bubble'] = drought.get_bubbles()
+    return res
 
 
 @app.route('/', methods=['GET'])
